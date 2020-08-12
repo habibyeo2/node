4e06ce6ba1f7689545ccc40f2fd13c005b9cb041
habibyeo2 <habibyeo2@gmail.com>
2020-08-12 01:41:29 -0400

    yea

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 // in node we manually create the server that listens to requests from the browser
 
 const http = require('http')
+const fs = require('fs')
 
 //create server takes a callback function as its argument
 //the call back function takes two arguments
 //those two arguments are req(request) and res (response)
-const server = http.createServer(()=>{
-    console.log('request made')
+const server = http.createServer((req,res)=>{
+    // req and res are both objects for the request and response
+    // console.log(req.url,req.method);
+
+    res.setHeader('Content-type', 'text-html')
+
+    //use a switch to set our url/path
+    let path ='./views/';
+    switch (req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+           path += 'about.html'
+           res.statusCode = 301;
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+
+    }
+    fs.readFile(path,(err,data)=>{
+        if (err){
+            console.log(err);
+            res.end()
+
+        }else{
+            // res.write(data)
+            //this passes the index.html into our write method
+            res.end(data)
+        }
+    })
 })
 
 //server.listen takes a portnumber and hostname and callback function as arguments
